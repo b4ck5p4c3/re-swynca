9e101bd4f8afbd9f8b9728284aa4c090b12caf91
Danil Bezborodov <xanmilanto@gmail.com>
2024-09-11 23:34:00 +0300

    add: space-transactions

--- backend/src/space-transactions/space-transactions.controller.ts
+++ backend/src/space-transactions/space-transactions.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Param, Post} from "@nestjs/common";
+import {Body, Controller, Get, HttpException, HttpStatus, Param, Post} from "@nestjs/common";
 import {
     ApiBody, ApiCookieAuth,
     ApiDefaultResponse,
@@ -6,15 +6,22 @@ import {
     ApiOperation,
     ApiProperty,
     ApiTags,
-    ApiUnauthorizedResponse
 } from "@nestjs/swagger";
 import {TransactionType} from "../common/database/entities/common";
 import {IsDate, IsEnum, IsNotEmpty, IsNumber, IsNumberString, IsUUID} from "class-validator";
 import {
+    SpaceTransaction,
     SpaceTransactionDeposit,
     SpaceTransactionWithdrawal
 } from "../common/database/entities/space-transaction.entity";
 import {ErrorApiResponse} from "../common/api-responses";
+import { SpaceTransactionsService } from "./space-transactions.service";
+import { MembersService } from "src/members/members.service";
+import Decimal from "decimal.js";
+import { CustomValidationError } from "src/common/exceptions";
+import { MONEY_DECIMAL_PLACES, MONEY_PRECISION } from "src/common/money";
+import { MemberTransactionsController } from "src/member-transactions/member-transactions.controller";
+import { UserId } from "src/auth/user-id.decorator";
 
 class SpaceTransactionDTO {
     @ApiProperty({format: "uuid"})
@@ -76,6 +83,21 @@ class CreateSpaceTransactionDTO {
 @Controller("space-transactions")
 @ApiTags("space-transactions")
 export class SpaceTransactionsController {
+    constructor(private spaceTransactionsService: SpaceTransactionsService, private membersService: MembersService){
+    }
+    private static mapToDTO(spaceTransaction: SpaceTransaction): SpaceTransactionDTO {
+        return {
+            id: spaceTransaction.id,
+            type: spaceTransaction.type,
+            amount: spaceTransaction.amount.toString(),
+            comment: spaceTransaction.comment,
+            date: spaceTransaction.date.toISOString(),
+            source: spaceTransaction.source,
+            target: spaceTransaction.target,
+            actorId: spaceTransaction.actor.id,
+            createdAt: spaceTransaction.createdAt.toISOString()
+        };
+    }
     @Get()
     @ApiOperation({
         summary: "Get all space transactions"
@@ -90,7 +112,7 @@ export class SpaceTransactionsController {
         type: ErrorApiResponse
     })
     async findAll(): Promise<SpaceTransactionDTO[]> {
-        return [];
+        return (await this.spaceTransactionsService.findAll()).map(SpaceTransactionsController.mapToDTO);
     }
 
     @Get("actor/:memberId")
@@ -106,8 +128,8 @@ export class SpaceTransactionsController {
         description: "Erroneous response",
         type: ErrorApiResponse
     })
-    async findAllByActor(@Param("memberId") memberId: string): Promise<SpaceTransactionDTO[]> {
-        return [];
+    async findAllByActor(@Param("memberId") actorId: string): Promise<SpaceTransactionDTO[]> {
+        return (await this.spaceTransactionsService.findAllByMemberId(actorId)).map(SpaceTransactionsController.mapToDTO);
     }
 
     @Post()
@@ -126,7 +148,29 @@ export class SpaceTransactionsController {
         description: "Erroneous response",
         type: ErrorApiResponse
     })
-    async create(@Body() request: CreateSpaceTransactionDTO): Promise<SpaceTransactionDTO> {
-        throw new Error();
-    }
+    async create(@UserId() actorId: string, @Body() request: CreateSpaceTransactionDTO): Promise<SpaceTransactionDTO> {
+        const {type, amount, comment, date, source, target} = request;
+        const actor = await this.membersService.findById(actorId);
+        if (!actor) {
+            throw new HttpException("Actor not found", HttpStatus.NOT_FOUND);
+        }
+        const decimalAmount = new Decimal(amount).toDecimalPlaces(MONEY_DECIMAL_PLACES);
+        if (decimalAmount.lessThanOrEqualTo(0)) {
+            throw new CustomValidationError("Membership amount must be > 0");
+        }
+        if (decimalAmount.precision() > MONEY_PRECISION - MONEY_DECIMAL_PLACES) {
+            throw new CustomValidationError(`Membership amount must be < 10^${MONEY_PRECISION - MONEY_DECIMAL_PLACES}`);
+        }
+
+        return SpaceTransactionsController.mapToDTO(await this.spaceTransactionsService.create({
+            type,
+            amount: decimalAmount,
+            comment,
+            date: new Date(date),
+            source,
+            target,
+            createdAt: new Date(),
+            actor
+        }));
+    };
 }
--- backend/src/space-transactions/space-transactions.service.ts
+++ backend/src/space-transactions/space-transactions.service.ts
@@ -1,6 +1,27 @@
 import {Injectable} from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { SpaceTransaction } from "src/common/database/entities/space-transaction.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class SpaceTransactionsService {
+    constructor(@InjectRepository(SpaceTransaction) private spaceTransactionRepository: Repository<SpaceTransaction>) {
+    }
 
+    async findAll(): Promise<SpaceTransaction[]> {
+        return await this.spaceTransactionRepository.find();
+    }
+
+    async findAllByMemberId(id: string): Promise<SpaceTransaction[] | null> {
+        return await this.spaceTransactionRepository.find({
+            where: {
+                id
+            }
+        });
+    }
+    async create(spaceTransaction: Omit<SpaceTransaction, "id">): Promise<SpaceTransaction> {
+        const createdSpaceTransaction = this.spaceTransactionRepository.create(spaceTransaction);
+        await this.spaceTransactionRepository.save(createdSpaceTransaction);
+        return createdSpaceTransaction;
+    }
 }
