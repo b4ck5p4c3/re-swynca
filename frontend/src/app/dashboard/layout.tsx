80f9205675b88415f23996bb1e883ac3be26bc4e
Egor Koleda <radioegor146@ctf.by>
2025-01-15 07:59:55 +0300

    fix

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({children,}: Readonly<{ children: React.
             <div className={"w-9/12 m-auto flex flex-col gap-8 mb-10"}>
                 <div className={"border-b-2 border-b-gray-300"}>
                     <div className={"flex flex-row pt-5 pb-5 items-center gap-4 justify-center"}>
-                        <div className={"text-4xl font-bold flex-row items-center gap-4 flex-1 hidden xs:flex cursor-pointer"} onClick={() => router.push("/")}>
+                        <div className={"text-4xl font-bold flex-row items-center gap-4 flex-1 hidden xs:flex cursor-pointer"} onClick={() => router.push("/dashboard")}>
                             <Image src={logo} alt={"logo"} width={40} className={"min-w-[40px]"}/>
                             <span className={"hidden 2xl:inline"}>RE: Swynca</span>
                         </div>
