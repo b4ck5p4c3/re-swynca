cc23e21f1e553b1301ad26309eb06a8ca505e2cb
Egor Koleda <radioegor146@ctf.by>
2025-04-05 19:12:47 +0300

    add auth token

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,6 +7,7 @@ export default withSentryConfig(nextConfig, {
     org: "bksp",
     project: "swynca",
     sentryUrl: "https://sentry.p.bksp.in/",
+    authToken: process.env.SENTRY_AUTH_TOKEN,
 
     silent: !process.env.CI,
 
